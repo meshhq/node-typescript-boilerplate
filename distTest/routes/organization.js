200f32e25f3f930117564be8c9856f78a4b54352
misstonbon <plusdeltah@gmail.com>
2018-10-15 10:13:33 -0700

    adds distTest to gitignore

--- distTest/routes/organization.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const organization_1 = require("../controllers/organization");
-function createOrganizationRoutes(router) {
-    router.post('/organizations', organization_1.default.createOrganization);
-    router.delete('/organizations/:organization_id', organization_1.default.deleteOrganization);
-}
-exports.default = createOrganizationRoutes;
